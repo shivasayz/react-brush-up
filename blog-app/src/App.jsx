8312aa019d6439820264d93dae060f5d6dcaec44
shivasayz <writeforshivaa@gmail.com>
2025-12-05 18:00:42 +0530

    redux setup

--- blog-app/src/App.jsx
+++ blog-app/src/App.jsx
@@ -1,10 +1,41 @@
 import "./App.css";
+import { Header, Footer } from "./components/index";
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  return (
-    <>
-      <h1>App write project</h1>
-    </>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          {/* <Outlet/> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div>Loading...</div>
   );
 }
 
